06be0547a841b524aa5084953f770d367b3fc475
Brian Krueger <briankrueger@northwesternmutual.com>
2018-11-06 13:15:47 -0600

    Refactored scss location for webpack

--- dogfinder/src/index.js
+++ dogfinder/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './css/scss/styles.scss';
-//import './css/compiled/styles.css';
+import './scss/styles.scss';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import configureStore from './store/configureStore';
